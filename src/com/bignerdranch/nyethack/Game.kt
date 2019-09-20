aa986b67fe8719c3ff1572e0ba7e049b5f5a836b
Jitendar Mandali <jitendar7@gmail.com>
2019-09-20 22:48:32 +0400

    end of chapter 12

---
+++ src/com/bignerdranch/nyethack/Game.kt
@@ -0,0 +1,27 @@
+package com.bignerdranch.nyethack
+
+const val MAX_EXPERIENCE: Int = 5000
+
+fun main(args: Array<String>) {
+
+    val player = Player()
+    player.castFireball()
+
+    val auraColor = player.auraColor()
+    val healthStatus = player.formatHealthStatus()
+
+    printPlayerStatus(auraColor, isBlessed = true)
+
+    val race = "gnome"
+    val faction = when(race){
+        "dwarf" -> "Keepers of the Mines"
+        "gnome" -> "Keepers of the Mines"
+         else -> "Free people of the Rolling Hills"
+    }
+
+}
+
+private fun printPlayerStatus(auraColor: String, isBlessed: Boolean) {
+    println("(Aura: $auraColor) " +
+            "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+}
